584b01cd13006291504c050919426aa6aa978be9
lys122519 <514955048@qq.com>
2022-05-30 15:10:24 +0800

    Initial commit

---
+++ src/main/java/com/sf/SpeakFreelyApplication.java
@@ -0,0 +1,13 @@
+package com.sf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakFreelyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeakFreelyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sf/SpeakFreelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakFreelyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
