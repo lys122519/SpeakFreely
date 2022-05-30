035bcbc766ceb403627c493059ad9f3bdecc83ea
lys122519 <514955048@qq.com>
2022-05-30 17:13:35 +0800

    sql文件添加

---
+++ src/main/resources/speakfreely.sql
@@ -0,0 +1,163 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80028
+ Source Host           : localhost:3306
+ Source Schema         : speakfreely
+
+ Target Server Type    : MySQL
+ Target Server Version : 80028
+ File Encoding         : 65001
+
+ Date: 30/05/2022 17:13:06
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for tb_article
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_article`;
+CREATE TABLE `tb_article`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `user_id` int(0) NULL DEFAULT NULL COMMENT '用户id',
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '标题',
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '内容',
+  `time` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `deteled` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_article
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_comment`;
+CREATE TABLE `tb_comment`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `user_id` int(0) NOT NULL COMMENT '评论用户id',
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '评论内容',
+  `time` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '评论时间',
+  `pid` int(0) NULL DEFAULT NULL COMMENT '评论父id',
+  `origin_id` int(0) NULL DEFAULT NULL COMMENT '最上级id',
+  `article_id` int(0) NOT NULL COMMENT '所属文章id',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_comment
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_file
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_file`;
+CREATE TABLE `tb_file`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件名称',
+  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件类型',
+  `size` bigint(0) NULL DEFAULT NULL COMMENT '文件大小 kb\r\n',
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件url',
+  `md5` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '文件md5',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '是否已删除',
+  `enabled` tinyint(1) NULL DEFAULT 1 COMMENT '是否禁用',
+  PRIMARY KEY (`id`, `md5`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_file
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_report
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_report`;
+CREATE TABLE `tb_report`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `user_id` int(0) NULL DEFAULT NULL COMMENT '举报人ID',
+  `article_id` int(0) NULL DEFAULT NULL COMMENT '文章ID',
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '举报理由',
+  `deleted` tinyint(1) NULL DEFAULT 0 COMMENT '是否删除',
+  `execd` tinyint(1) NULL DEFAULT 0 COMMENT '是否已处理',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_report
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_role
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_role`;
+CREATE TABLE `tb_role`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '名称',
+  `description` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '描述',
+  `flag` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '唯一标识',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_role
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_tag_article
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_tag_article`;
+CREATE TABLE `tb_tag_article`  (
+  `tga_id` int(0) NOT NULL COMMENT '标签id',
+  `article_id` int(0) NOT NULL COMMENT '文章id',
+  PRIMARY KEY (`tga_id`, `article_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_tag_article
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_tags
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_tags`;
+CREATE TABLE `tb_tags`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `content` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '标签内容',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_tags
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tb_user
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_user`;
+CREATE TABLE `tb_user`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名',
+  `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `nickname` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '昵称',
+  `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱',
+  `phone` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '手机号',
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '地址',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `avatar_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '头像url',
+  `role` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '角色',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_user
+-- ----------------------------
+INSERT INTO `tb_user` VALUES (1, 'lys122519', 'lys122519', 'lys122519', '514955048@qq.com', '18064373749', '陕西西安', '2022-05-30 16:10:17', NULL, 'ROLE_ADMIN');
+
+SET FOREIGN_KEY_CHECKS = 1;
