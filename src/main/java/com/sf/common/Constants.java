d0b200763726b4d8e94c409a08f38b981b326693
lys122519 <514955048@qq.com>
2022-05-30 16:14:14 +0800

    user 模块初步

---
+++ src/main/java/com/sf/common/Constants.java
@@ -0,0 +1,34 @@
+package com.sf.common;
+
+/**
+ * @Description:
+ * @author: leung
+ * @date: 2022-03-27 14:15
+ */
+public interface Constants {
+    /**
+     * 系统错误
+     */
+    String CODE_500 = "500";
+    /**
+     * 请求成功
+     */
+    String CODE_200 = "200";
+
+    /**
+     * 参数错误
+     */
+    String CODE_400 = "400";
+
+    /**
+     * 权限不足
+     */
+    String CODE_401 = "401";
+
+    /**
+     * 业务异常
+     */
+    String CODE_600 = "600";
+
+
+}
---
+++ src/main/java/com/sf/common/Result.java
@@ -0,0 +1,71 @@
+package com.sf.common;
+
+/**
+ * @Description: 接口统一返回
+ * @author: leung
+ * @date: 2022-03-27 14:12
+ */
+public class Result {
+
+    /**
+     * 操作是否成功
+     */
+    private String code;
+    /**
+     * msg
+     */
+    private String msg;
+    /**
+     * 数据
+     */
+    private Object data;
+
+    public Result() {
+    }
+
+    public Result(String code, String msg, Object data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public static Result success() {
+        return new Result(Constants.CODE_200, "", null);
+    }
+
+    public static Result success(Object data) {
+        return new Result(Constants.CODE_200, "", data);
+    }
+
+    public static Result error(String code, String msg) {
+        return new Result(code, msg, null);
+    }
+
+    public static Result error() {
+        return new Result(Constants.CODE_500, "系统错误", null);
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
