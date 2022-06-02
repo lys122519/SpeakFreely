51eb944f70b0194ebce3bb93ce1791daaf9b8c59
lys122519 <514955048@qq.com>
2022-06-02 13:58:36 +0800

    add  用户身份

---
+++ src/main/java/com/sf/common/RoleEnum.java
@@ -0,0 +1,17 @@
+package com.sf.common;
+
+/**
+ * @Description:
+ * @author: leung
+ * @date: 2022-06-02 13:57
+ */
+public enum RoleEnum {
+    /**
+     * 管理员
+     */
+    ROLE_ADMIN,
+    /**
+     * 普通用户
+     */
+    ROLE_USER
+}
