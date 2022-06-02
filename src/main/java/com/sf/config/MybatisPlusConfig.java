4f43b393026b1fd081e2b5a1dde3b2d59d759167
lys122519 <514955048@qq.com>
2022-06-03 00:13:35 +0800

    mybatisplus乐观锁插件配置

--- src/main/java/com/sf/config/MybatisPlusConfig.java
+++ src/main/java/com/sf/config/MybatisPlusConfig.java
@@ -2,6 +2,7 @@ package com.sf.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +23,8 @@ public class MybatisPlusConfig {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         //分页插件
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        //乐观锁
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         return interceptor;
     }
 }
